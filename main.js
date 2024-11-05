d92dd391e00c36690c1273ecc583d38b8ec54ba8
Asho09 <aash69115@gmail.com>
2024-11-06 02:15:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Question 02
+//Personal meesage : Store a person name in a variable ,and print a message to that
+// person.You message should be simple such as,"hello eric ,would you like to learn
+// some pythons today?"
+var personName = "Ayesha";
+console.log("hello", personName, "would you like to learn some pythons today?");
---
+++ main.ts
@@ -0,0 +1,8 @@
+//Question 02
+//Personal meesage : Store a person name in a variable ,and print a message to that
+// person.You message should be simple such as,"hello eric ,would you like to learn
+// some pythons today?"
+
+
+let personName="Ayesha";
+console.log("hello",personName,"would you like to learn some pythons today?");
